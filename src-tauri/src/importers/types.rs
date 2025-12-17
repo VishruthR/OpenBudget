b4906860b2065aea3b8e7264dca8a0395641c85b
Vishruth Raj <vishraj248@gmail.com>
2025-12-17 17:01:11 -0600

    Use csv + serde to improve parsing for the most part, still some small stuff to clean up

--- src-tauri/src/importers/types.rs
+++ src-tauri/src/importers/types.rs
@@ -1,14 +1,42 @@
 use std::fmt;
+use chrono::NaiveDate;
+use serde::{Deserialize, Deserializer};
 
-#[derive(Debug, PartialEq, Eq, Clone)]
+#[derive(Debug, PartialEq, Clone, serde::Deserialize)]
 pub struct Transaction {
-    pub date: String,
+    #[serde(deserialize_with = "de_date_from_str")]
+    pub date: NaiveDate,
+    #[serde(deserialize_with = "de_description_from_str")]
     pub description: String,
-    pub amount: String,
+    #[serde(deserialize_with = "de_float_from_money_str")]
+    pub amount: f64,
 }
 
 impl fmt::Display for Transaction {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Transaction: {}, {}, {}", self.date, self.description, self.amount)
     }
+}
+
+fn de_date_from_str<'de, D>(deserializer: D) -> Result<NaiveDate, D::Error>
+    where D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    NaiveDate::parse_from_str(&s, "%m/%d/%Y").map_err(serde::de::Error::custom)
+}
+
+fn de_description_from_str<'de, D>(deserializer: D) -> Result<String, D::Error>
+    where D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    Ok(s[1..s.len() - 1].to_owned())
+}
+
+// We need a custom deserializer since banks often include thousands seperators
+fn de_float_from_money_str<'de, D>(deserializer: D) -> Result<f64, D::Error>
+    where D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    let clean = s.replace(',', "").replace('\"', "").trim().to_string();
+    clean.parse::<f64>().map_err(serde::de::Error::custom)
 }
