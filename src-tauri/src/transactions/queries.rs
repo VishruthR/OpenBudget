a1981c87639f9b11b08f9b38bcef5047580aa59d
Vishruth Raj <vishraj248@gmail.com>
2025-12-20 14:52:10 -0600

    Get initial implementation of get_all_transactions working with test case

--- src-tauri/src/transactions/queries.rs
+++ src-tauri/src/transactions/queries.rs
@@ -1,15 +1,13 @@
 use sqlx::{Pool, Sqlite};
-use crate::types::Transaction;
+use crate::types::{Transaction, Cents};
+use chrono::NaiveDate;
+use rust_decimal::dec;
 
-#[tokio::main]
 pub async fn get_transactions(pool: &Pool<Sqlite>, limit: Option<u32>) -> Result<Vec<Transaction>, sqlx::Error> {
-    let mut query = "SELECT * FROM transaction ORDER BY date".to_owned();
-    if let Some(lim) = limit {
-        query.push_str(" LIMIT '%' || $1 || '%'");
-    }
+    // TODO: Look into supporting limit using prepared statements, maybe have to do in separate function
+    let mut query = "SELECT * FROM 'transaction' ORDER BY date, id".to_owned();
 
     let res: Vec<Transaction> = sqlx::query_as(query.as_str())
-        .bind(lim)
         .fetch_all(pool).await?;
 
     Ok(res)
@@ -19,8 +17,46 @@ pub async fn get_transactions(pool: &Pool<Sqlite>, limit: Option<u32>) -> Result
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_get_all_transactions() -> Result<(), Box<dyn std::error::Error>> {
+    #[sqlx::test(fixtures(path="../fixtures", scripts("transactions")))]
+    async fn test_get_all_transactions(pool: Pool<Sqlite>) -> Result<(), Box<dyn std::error::Error>> {
+        let transactions_expected = vec![
+            Transaction::new(
+                1,
+                "TRANSACTION 1".to_owned(),
+                Cents(dec!(-5.77)),
+                NaiveDate::from_ymd_opt(2025, 12, 15).unwrap(),
+                1,
+                1
+            ),
+            Transaction::new(
+                2,
+                "TRANSACTION 2".to_owned(),
+                Cents(dec!(-10.90)),
+                NaiveDate::from_ymd_opt(2025, 12, 16).unwrap(),
+                1,
+                1
+            ),
+            Transaction::new(
+                4,
+                "TRANSACTION 4".to_owned(),
+                Cents(dec!(-0.70)),
+                NaiveDate::from_ymd_opt(2025, 12, 16).unwrap(),
+                1,
+                1
+            ),
+            Transaction::new(
+                3,
+                "TRANSACTION 3".to_owned(),
+                Cents(dec!(-1.90)),
+                NaiveDate::from_ymd_opt(2025, 12, 17).unwrap(),
+                1,
+                1
+            ),
+        ];
+
+        let transactions = get_transactions(&pool, Some(0)).await?;
+        
+        assert_eq!(transactions, transactions_expected);
         Ok(())
     }
 }
